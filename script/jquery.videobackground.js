7d3586ea2b738177eb8ee0ab404445eb9e1d33da
georgepaterson <george.paterson@lbi.com>
2011-08-21 19:54:31 +0100

    Created public string methods allowing direct access to play and mute. Destroy method in development. Defaults settings remain the same.

--- script/jquery.videobackground.js
+++ script/jquery.videobackground.js
@@ -8,259 +8,282 @@
  */
 (function($){
 	/*
-	 * The videobackground plugin will place a resizable HTML5 video in to a designated element.
-	 * This may be used as a background for the document or a specific element.
+	 *
+	 */
+	var methods = {
+		/*
+		 *
+		 */
+		init: function (options) {
+			if (document.createElement('video').canPlayType) {	
+			  return this.each(function () {
+					var element = $(this);
+					element.settings = $.extend({}, $.fn.videobackground.defaults, options);
+					/*
+					 *	If the resize option is set.
+					 *	Set the height of the container to be the height of the document
+					 *	The video can expand in to the space using min-height: 100%;
+					 *
+					 */
+					if (element.settings.resize) {
+						resize(element);
+					}
+					/*
+					 *	Complie the different HTML5 video attributes.	
+					 *
+					 */
+					var compiledSource = '';
+					$.each(element.settings.videoSource, function(index, value) { 
+					  compiledSource = compiledSource + '<source src="' + value + '">';
+					});
+					var attributes = '';
+					attributes = attributes + 'preload="' + element.settings.preload + '"';
+					if (element.settings.poster) {
+						attributes = attributes + ' poster="' + element.settings.poster + '"';
+					}
+					if (element.settings.autoplay) {
+						attributes = attributes + ' autoplay="autoplay"';
+					}
+					if (element.settings.loop) {
+						attributes = attributes + ' loop="loop"';
+					}
+					$(element).html('<video '+attributes+'>' + compiledSource + '</video>');
+					/*
+					 * Append the control box either to the supplied element or the video background element.	
+					 *
+					 */
+					element.controlbox = $('<div class="ui-video-background ui-widget ui-widget-content ui-corner-all"></div>');
+					if (element.settings.controlPosition) {
+						$(element.settings.controlPosition).append(element.controlbox);
+					}
+					else {
+						$(element).append(element.controlbox);
+					}
+					/*
+					 *	HTML string for the video controls.
+					 *
+					 */
+					element.controls = $('<ul class="ui-video-background-controls"><li class="ui-video-background-play">'
+						+ '<a class="ui-icon ui-icon-pause" href="#">'+element.settings.controlText[1]+'</a>'
+						+ '</li><li class="ui-video-background-mute">'
+						+ '<a class="ui-icon ui-icon-volume-on" href="#">'+element.settings.controlText[2]+'</a>'
+						+ '</li></ul>');		
+					/*
+					 *	
+					 *
+					 */
+					
+					if (element.settings.preloadHtml || element.settings.preloadCallback) {
+						preload(element);
+						$('video', element).bind('canplaythrough', function() {
+							/*
+							 * Chrome doesn't currently using the autoplay attribute.
+							 * Autoplay initiated through JavaScript.
+							 *
+							 */
+							if (element.settings.autoplay) {		
+									$('video', element).get(0).play();
+							}
+							loaded(element);
+						});
+					}
+					else {
+						$('video', element).bind('canplaythrough', function() {
+							/*
+							 * Chrome doesn't currently using the autoplay attribute.
+							 * Autoplay initiated through JavaScript.
+							 *
+							 */
+							if (element.settings.autoplay) {		
+									$('video', element).get(0).play();
+							}
+							loaded(element);
+						});
+					}
+		  	});
+			}
+			else {
+				return this.each(function () {
+					var element = $(this);
+					element.settings = $.extend({}, $.fn.videobackground.defaults, options);
+					if (element.settings.poster) {
+						var image = $('<img src="'+ element.settings.poster +'">');
+						element.append(image);
+					}
+				});
+			}
+		},
+		/*
+		 *
+		 */
+		play: function (options) {
+		  return this.each(function () {
+				var element = $(this);
+				element.settings = $.extend({}, $.fn.videobackground.defaults, options);
+				play(element);
+		  });
+		},
+		/*
+		 *
+		 */
+		mute: function (options) {
+		  return this.each(function () {
+				var element = $(this);
+				element.settings = $.extend({}, $.fn.videobackground.defaults, options);
+				mute(element);
+		  });
+		},
+		/*
+		 *
+		 */
+		destroy: function (options) {
+		  return this.each(function () {
+				var element = $(this);
+				element.settings = $.extend({}, $.fn.videobackground.defaults, options);
+				
+				
+		  });
+		}
+  };
+	/*
+	 *	
+	 *
+	 */
+	function resize (element) {
+		var documentHeight = $(document).height(),
+			windowHeight = $(window).height();
+		if (windowHeight >= documentHeight) {
+			$(element).css('height', windowHeight);
+		}
+		else if (documentHeight > windowHeight) {
+			$(element).css('height', documentHeight);
+		}
+	};
+	/*
+	 *	
+	 *
+	 */
+	function preload (element) {
+		$(controlbox).append(preloadHtml);
+		if (preloadCallback) {
+			(preloadCallback).call(element);
+		}
+	};
+	/*
+	 *	
+	 *
+	 */
+	function loaded (element) {
+		$(element.controlbox).html(element.controls);
+		loadedEvents(element);
+		if (element.settings.loadedCallback) {
+			(element.settings.loadedCallback).call(element);
+		}
+	};
+	/*
+	 *	
+	 *
+	 */
+	function loadedEvents (element) {
+		/*
+		 * Trigger the resize method based if the browser is resized.
+		 *
+		 */
+		if (element.settings.resize) {
+			$(window).resize(function () {
+				resize(element);
+			});
+		}
+		/*
+		 *	Play/pause control	
+		 *
+		 */
+		$('.ui-video-background-play a', element.controls).click(function(event) {
+			event.preventDefault();
+			play(element);
+		});
+		/*
+		 *	Mute/unmute control	
+		 *
+		 */
+		$('.ui-video-background-mute a', element.controls).click(function(event) {
+			event.preventDefault();
+			mute(element);
+		});
+		/*
+		 * Firefox doesn't currently use the loop attribute.
+		 * Loop bound to the video ended event.
+		 *
+		 */	
+		if (element.settings.loop) {		
+			$('video', element).bind('ended', function(){ 
+				$('video', element).get(0).play();
+				$(this).toggleClass('paused');
+				$(this).text(element.settings.controlText[1]);
+			});
+		}
+	};
+	/*
+	 *	
+	 *
+	 */
+	function play (element) {
+		if ($('video', element).get(0).paused) {
+			$('video', element).get(0).play();
+			$('.ui-video-background-play a', element.controls).toggleClass('ui-icon-pause ui-icon-play');
+			$('.ui-video-background-play a', element.controls).text(element.settings.controlText[1]);
+		} 
+		else {
+			if ($('video', element).get(0).ended) {
+				$('video', element).get(0).play();
+				$('.ui-video-background-play a', element.controls).toggleClass('ui-icon-pause ui-icon-play');
+				$('.ui-video-background-play a', element.controls).text(element.settings.controlText[1]);
+			}
+			else {
+				$('video', element).get(0).pause();
+				$('.ui-video-background-play a', element.controls).toggleClass('ui-icon-pause ui-icon-play');
+				$('.ui-video-background-play a', element.controls).text(element.settings.controlText[0]);
+			}
+		}
+	};
+	/*
+	 *	
+	 *
+	 */
+	function mute (element) {
+		if ($('video', element).get(0).volume === 0) {
+			$('video', element).attr('muted', false);
+			$('video', element).get(0).volume = 1;
+			$('.ui-video-background-mute a', element.controls).toggleClass('ui-icon-volume-on ui-icon-volume-off');
+			$('.ui-video-background-mute a', element.controls).text(element.settings.controlText[2]);
+		} 
+		else {
+			$('video', element).attr('muted', true);
+			$('video', element).get(0).volume = 0;
+			$('.ui-video-background-mute a', element.controls).toggleClass('ui-icon-volume-on ui-icon-volume-off');
+			$('.ui-video-background-mute a', element.controls).text(element.settings.controlText[3]);
+		}
+	};
+	/*
+	 *	
 	 *
 	 */
 	$.fn.videobackground = function (method) {
-		/*
-		 * Public methods.
-		 *
-		 */
-		var methods = {
-			/* 
-			 * Initialise the plugin.
-			 *
-			 */
-			init: function (options) {
-				this.videobackground.settings = $.extend({}, this.videobackground.defaults, options);
-				if (document.createElement('video').canPlayType) {
-					return this.each(function() {
-						element = $(this);
-						members.create();
-					});
-				}
-				else {
-					if (this.videobackground.settings.poster) {
-						var image = $('<img src="'+ this.videobackground.settings.poster +'">');
-						$(this).append(image);
-					}
-				}
-			},
-			/*
-			 * Destroy the plugin.
-			 *
-			 */
-			destroy: function () {
-
-			}
-		};
-		/*
-		 * Private members.
-		 *
-		 */
-		var members = {
-			/*
-			 *	Create the video and controls.
-			 *
-			 */
-			create: function () {
-				/*
-				 *	If the resize option is set.
-				 *	Set the height of the container to be the height of the document
-				 *	The video can expand in to the space using min-height: 100%;
-				 *
-				 */
-				if (element.videobackground.settings.resize) {
-					members.resizeVideo();
-				}
-				/*
-				 *	Complie the different HTML5 video attributes.	
-				 *
-				 */
-				var compiledSource = '',
-					i = 0,
-					j = 0;
-				for (i = 0,  j = element.videobackground.settings.videoSource.length; i < j; i++) {
-					compiledSource = compiledSource + '<source src="' + element.videobackground.settings.videoSource[i] + '">';
-				}
-				var attributes = '';
-				attributes = attributes + 'preload="' + element.videobackground.settings.preload + '"';
-				if (element.videobackground.settings.poster) {
-					attributes = attributes + ' poster="' + element.videobackground.settings.poster + '"';
-				}
-				if (element.videobackground.settings.autoplay) {
-					attributes = attributes + ' autoplay="autoplay"';
-				}
-				if (element.videobackground.settings.loop) {
-					attributes = attributes + ' loop="loop"';
-				}
-				$(element).html('<video '+attributes+'>' + compiledSource + '</video>');
-				/*
-				 * Append the control box either to the supplied element or the video background element.	
-				 *
-				 */
-				controlbox = $('<div class="ui-video-background ui-widget ui-widget-content ui-corner-all"></div>');
-				if (element.videobackground.settings.controlPosition) {
-					$(element.videobackground.settings.controlPosition).append(controlbox);
-				}
-				else {
-					$(element).append(controlbox);
-				}
-				/*
-				 *	HTML string for the video controls.
-				 *
-				 */
-				controls = $('<ul class="ui-video-background-controls"><li class="ui-video-background-play"><a class="ui-icon ui-icon-pause" href="#">'+element.videobackground.settings.controlText[1]+'</a></li><li class="ui-video-background-mute"><a class="ui-icon ui-icon-volume-on" href="#">'+element.videobackground.settings.controlText[2]+'</a></li></ul>');		
-				/*
-				 *	
-				 *
-				 */
-				if (element.videobackground.settings.preloadHtml || element.videobackground.settings.preloadCallback) {
-					members.preload();
-					$('video', element).bind('canplaythrough', function() {
-						/*
-						 * Chrome doesn't currently using the autoplay attribute.
-						 * Autoplay initiated through JavaScript.
-						 *
-						 */
-						if (element.videobackground.settings.autoplay) {		
-								$('video', element).get(0).play();
-						}
-						members.loaded();
-					});
-				}
-				else {
-					$('video', element).bind('canplaythrough', function() {
-						/*
-						 * Chrome doesn't currently using the autoplay attribute.
-						 * Autoplay initiated through JavaScript.
-						 *
-						 */
-						if (element.videobackground.settings.autoplay) {		
-								$('video', element).get(0).play();
-						}
-						members.loaded();
-					});
-				}
-			},
-			/*
-			 *	
-			 *
-			 */
-			preload: function() {
-				$(controlbox).append(element.videobackground.settings.preloadHtml);
-				if (element.videobackground.settings.preloadCallback) {
-					(element.videobackground.settings.preloadCallback).call(this);
-				}
-			},
-			/*
-			 *	
-			 *
-			 */
-			loaded: function() {
-				$(controlbox).html(controls);
-				members.loadedEvents();
-				if (element.videobackground.settings.loadedCallback) {
-					(element.videobackground.settings.loadedCallback).call(this);
-				}
-			},
-			/*
-			 *	Will resize the video height based on either the highest window or document	height.
-			 *
-			 */
-			resizeVideo: function() {
-				var element = $(this);
-				var documentHeight = $(document).height(),
-				windowHeight = $(window).height();
-				if (windowHeight >= documentHeight) {
-					$(element).css('height', windowHeight);
-				}
-				else if (documentHeight > windowHeight) {
-					$(element).css('height', documentHeight);
-				}
-			},
-			/*
-			 *	Video events.
-			 *
-			 */
-			loadedEvents: function() {
-				/*
-				 * Will trigger the resize method based on the browser being resized.
-				 *
-				 */
-				if (element.videobackground.settings.resize) {
-					$(window).resize(function() {
-						members.resizeVideo();
-					});
-				}
-				/*
-				 *	Play/pause control	
-				 *
-				 */
-				$('.ui-video-background-play a', controls).click(function(event) {
-					event.preventDefault();
-					if ($('video', element).get(0).paused) {
-						$('video', element).get(0).play();
-						$(this).toggleClass('ui-icon-pause ui-icon-play');
-						$(this).text(element.videobackground.settings.controlText[1]);
-					} 
-					else {
-						if ($('video', element).get(0).ended) {
-							$('video', element).get(0).play();
-							$(this).toggleClass('ui-icon-pause ui-icon-play');
-							$(this).text(element.videobackground.settings.controlText[1]);
-						}
-						else {
-						$('video', element).get(0).pause();
-						$(this).toggleClass('ui-icon-pause ui-icon-play');
-						$(this).text(element.videobackground.settings.controlText[0]);
-						}
-					}
-				});
-				/*
-				 *	Mute/unmute control	
-				 *
-				 */
-				$('.ui-video-background-mute a', controls).click(function(event) {
-					event.preventDefault();
-					if ($('video', element).attr('muted')) {
-						$('video', element).attr('muted', false);
-						$('video', element).get(0).volume = 1;
-						$(this).toggleClass('ui-icon-volume-on ui-icon-volume-off');
-						$(this).text(element.videobackground.settings.controlText[2]);
-					} 
-					else {
-						$('video', element).attr('muted', true);
-						$('video', element).get(0).volume = 0;
-						$(this).toggleClass('ui-icon-volume-on ui-icon-volume-off');
-						$(this).text(element.videobackground.settings.controlText[3]);
-					}
-				});	
-				/*
-				 * Firefox doesn't currently use the loop attribute.
-				 * Loop bound to the video ended event.
-				 *
-				 */	
-				if (element.videobackground.settings.loop) {		
-					$('video', element).bind('ended', function(){ 
-						$('video', element).get(0).play();
-						$(this).toggleClass('paused');
-						$(this).text(element.videobackground.settings.controlText[1]);
-					});
-				}
-			}
-		};
 		/*
 		 *	Allow for method calling.
 		 *	If not a method initialise the plugin.
 		 *
 		 */
-		if (methods[method]) {
-			return methods[method].apply(this, Array.prototype.slice.call(arguments, 1));
-		} 
+    if (methods[method]) {
+      return methods[method].apply(this, Array.prototype.slice.call(arguments, 1));
+    } 
 		else if (typeof method === 'object' || !method) {
-			return methods.init.apply(this, arguments);
-		} 
+      return methods.init.apply(this, arguments);
+    } 
 		else {
-			$.error('Method ' +  method + ' does not exist on jQuery.personalisation.');
-		}
+      $.error('Method ' +  method + ' does not exist on jQuery.videobackground');
+    }
 	};
-	/*
-	 *	Defaults combined with options form the plugin settings.
-	 *
-	 */
-	$.fn.videobackground.settings = {};
 	/*
 	 *	Default options, can be extend by options passed to the function.
 	 *
