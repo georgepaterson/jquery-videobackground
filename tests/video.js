3a4c0b808b20ac8d27378f2ebe8ffc377d972047
George Paterson <paterson.george@googlemail.com>
2012-06-09 18:44:19 +0100

    Unit testing plugin methods.

--- tests/video.js
+++ tests/video.js
@@ -1,29 +1,56 @@
-$(document).ready( function () {
-	module("methods");
+$(document).ready(function () {
+	"use strict";
+	module('methods');
 	test('init', function () {
 		$('<div></div>').appendTo('body').videobackground({
 			videoSource: [['../video/big-buck-bunny.mp4', 'video/mp4'],
-				['../video/big-buck-bunny.webm', 'video/webm'], 
-				['../video/big-buck-bunny.ogv', 'video/ogg']], 
+				['../video/big-buck-bunny.webm', 'video/webm'],
+				['../video/big-buck-bunny.ogv', 'video/ogg']]
 		}).remove();
-		ok(true, '.videobackground() initiated on element with video type');
+		ok(true, '.videobackground() called on element with video type');
 		$('<div></div>').appendTo('body').videobackground({
-			videoSource: [['../video/big-buck-bunny.mp4'],
-				['../video/big-buck-bunny.webm'], 
-				['../video/big-buck-bunny.ogv']], 
+			videoSource: [['../video/big-buck-bunny.mp4', 'video/mp4'],
+				['../video/big-buck-bunny.webm', 'video/webm'],
+				['../video/big-buck-bunny.ogv', 'video/ogg']]
 		}).remove();
-		ok(true, '.videobackground() initiated on element without video type');
-	})
+		ok(true, '.videobackground() called on element without video type');
+		$([]).videobackground({
+			videoSource: [['../video/big-buck-bunny.mp4', 'video/mp4'],
+				['../video/big-buck-bunny.webm', 'video/webm'],
+				['../video/big-buck-bunny.ogv', 'video/ogg']]
+		}).remove();
+		ok(true, '.dialog() called on empty collection');
+	});
 	test('play', function () {
-
-	})
+		$('<div></div>').appendTo('body').videobackground({
+			videoSource: [['../video/big-buck-bunny.mp4', 'video/mp4'],
+				['../video/big-buck-bunny.webm', 'video/webm'],
+				['../video/big-buck-bunny.ogv', 'video/ogg']]
+		}).videobackground('play').remove();
+		ok(true, '.dialog(\'play\') called on element');
+	});
 	test('mute', function () {
-
-	})
+		$('<div></div>').appendTo('body').videobackground({
+			videoSource: [['../video/big-buck-bunny.mp4', 'video/mp4'],
+				['../video/big-buck-bunny.webm', 'video/webm'],
+				['../video/big-buck-bunny.ogv', 'video/ogg']]
+		}).videobackground('mute').remove();
+		ok(true, '.dialog(\'mute\') called on element');
+	});
 	test('resize', function () {
-
-	})
+		$('<div></div>').appendTo('body').videobackground({
+			videoSource: [['../video/big-buck-bunny.mp4', 'video/mp4'],
+				['../video/big-buck-bunny.webm', 'video/webm'],
+				['../video/big-buck-bunny.ogv', 'video/ogg']]
+		}).videobackground('resize').remove();
+		ok(true, '.dialog(\'resize\') called on element');
+	});
 	test('destroy', function () {
-
-	})
-})
+		$('<div></div>').appendTo('body').videobackground({
+			videoSource: [['../video/big-buck-bunny.mp4', 'video/mp4'],
+				['../video/big-buck-bunny.webm', 'video/webm'],
+				['../video/big-buck-bunny.ogv', 'video/ogg']]
+		}).videobackground('destroy').remove();
+		ok(true, '.dialog(\'destroy\') called on element');
+	});
+});
